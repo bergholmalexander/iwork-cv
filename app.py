8227f218fc6c520f08959cec9ce94bd6fe958129
Alexander Bergholm <bergholm.alexander@gmail.com>
2020-02-25 15:56:04 -0800

    Minor error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_restful import Resource, Api
 import requests
 import urllib
@@ -16,12 +16,25 @@ class HelloWorld(Resource):
 class GetCoordinates(Resource):
     def get(self, id):
         # TODO: Error checking i.e. id empty
-        url = urlHandler.getUrl(id)
-        image = urlHandler.getGDImage(url)
-        template = urlHandler.getGDImage('https://drive.google.com/uc?export=download&id=1FLqmm12Q3DEM_Ae7QfDHl8HOZeO_-Ffr')
-        points = detection.findPoints(image, 0.5, template)
-        print(points)
-        return {'coordinates': points}
+        if id == "":
+            abort(400, "Bad Request: No id was entered")
+        try:
+            url = urlHandler.getUrl(id)
+        except:
+            abort(404, "Not Found: Could not find url for given id")
+        try:
+            image = urlHandler.getGDImage(url)
+        except:
+            abort(404, "Not Found: Could not download an image at given url")
+        try:
+            template = urlHandler.getGDImage('https://drive.google.com/uc?export=download&id=1FLqmm12Q3DEM_Ae7QfDHl8HOZeO_-Ffr')
+        except:
+            abort(404, "Not Found: Could not download template at given url")
+        try:
+            points = detection.findPoints(image, 0.5, template)
+        except:
+            abort(500, "Internal Server Error: Template Matching algorithm failed")
+        return {'coordinates': points}, 200
 
 api.add_resource(HelloWorld, '/') # Landing page
 api.add_resource(GetCoordinates, '/floors/detect/<string:id>') # Landing page
