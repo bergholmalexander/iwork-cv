8944f5c16957d6d84baba50b653e02fe25791ba1
Alexander Bergholm <bergholm.alexander@gmail.com>
2020-03-24 17:53:39 -0700

    trying 1 instead of 0 for oem

--- ocr.py
+++ ocr.py
@@ -9,7 +9,7 @@ def bulkPointOCR(points, img, w, h):
     for p in points:
         match = cv2.cvtColor(img[p[1]-np.floor(h/2).astype(int):p[1]+np.floor(h/2).astype(int)+1, (p[0]-np.floor(w/2).astype(int)):p[0]+np.floor(w/2).astype(int)+1], cv2.COLOR_BGR2RGB)
         text = pytesseract.image_to_string(match, lang='eng',
-                            config='--psm 13 --oem 0 -c tessedit_char_whitelist=0123456789ABC')
+                            config='--psm 13 --oem 1 -c tessedit_char_whitelist=0123456789ABC')
         if text == "":
             print("cannot find anything in this case!")
         else:
