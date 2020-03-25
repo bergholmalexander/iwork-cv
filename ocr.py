b0ee56900c3497297e0be1dca31995e9d8dae002
Alexander Bergholm <bergholm.alexander@gmail.com>
2020-03-24 19:23:08 -0700

    testing

--- ocr.py
+++ ocr.py
@@ -7,7 +7,9 @@ def bulkPointOCR(points, img, w, h):
     # left here incase it needs to be tested again
     compiled = []
     for p in points:
+        print("before match")
         match = cv2.cvtColor(img[p[1]-np.floor(h/2).astype(int):p[1]+np.floor(h/2).astype(int)+1, (p[0]-np.floor(w/2).astype(int)):p[0]+np.floor(w/2).astype(int)+1], cv2.COLOR_BGR2RGB)
+        print("after match")
         text = pytesseract.image_to_string(match,
                             config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789ABC')
         if text == "":
