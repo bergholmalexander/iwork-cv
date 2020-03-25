4666e472731634c077b5f8828bcf651eff545f4c
Alexander Bergholm <bergholm.alexander@gmail.com>
2020-03-24 17:10:25 -0700

    attempt at upgade

--- ocr.py
+++ ocr.py
@@ -6,8 +6,11 @@ def bulkPointOCR(points, img, w, h):
     compiled = []
     for p in points:
         match = cv2.cvtColor(img[p[1]-np.floor(h/2).astype(int):p[1]+np.floor(h/2).astype(int)+1, (p[0]-np.floor(w/2).astype(int)):p[0]+np.floor(w/2).astype(int)+1], cv2.COLOR_BGR2RGB)
-        print("Everything working until pytess image to string?")
         text = pytesseract.image_to_string(match, lang='eng',
                             config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789ABC')
+        if text == "":
+            print("cannot find anything in this case!")
+        else:
+            print(text)
         compiled.append({"x": int(p[1]), "y": int(p[0]), "workspace_name": text})
     return compiled
