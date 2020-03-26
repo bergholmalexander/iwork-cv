6d4f6ee2d4ad0d543560e9e6b2997a7c7a9b498b
Alexander Bergholm <bergholm.alexander@gmail.com>
2020-03-25 22:44:14 -0700

    hardcode to fix for now

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ import cv2
 
 def getTemplates(path="templates/"):
     print(glob.glob(path+"*"))
-    return [cv2.imread(i, 0) for i in glob.glob(path+"*")]
-
+    return [cv2.imread(i, 0) for i in ['templates/templateloc2.jpg', 'templates/template3.jpg', 'templates/template1.jpg']] # On local ['templates/templateloc2.jpg', 'templates/template3.jpg', 'templates/template1.jpg']
+    #will fix to not be hardcoded later --> glob.glob(path+"*")
 def hwAverage(templates):
     s = [list(row) for row in [t.shape for t in templates]]
     hw = np.mean(s, axis=0)
